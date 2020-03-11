f6cc04a8a0e20f562323c49ea8fbd781a5d87247
gwTomS <60476919+gwTomS@users.noreply.github.com>
2020-03-11 14:13:59 +0000

    pull httrack and output to local
    script which pulls httrack image and runs it with a given website - website directories are currently hard coded.

---
+++ exportandclean.ps1
@@ -0,0 +1,23 @@
+﻿$givenURL = "http://visionfromabove.co.uk/"
+#$givenURL = "https://glasswallsolutions.com/"
+$sitename = ([System.Uri]$givenURL).Host -replace '^www\.'
+$outputedFilesDirectory = "C:\site\mirror"
+$parentPath = "C:\output\single\"
+
+docker run -it -v C:/site:/app -e HTTRACK_URI=$givenURL ralfbs/httrack:latest
+
+
+New-Item -ItemType directory (Join-Path $parentPath $sitename)
+
+$sourcepath = "C:\site\mirror\" 
+$destination = "C:\output\single\" + $sitename
+$sourcefiles = get-childitem $sourcepath  -file -Recurse
+
+foreach ($file in $sourcefiles) {
+Copy-Item $file.FullName -Destination "$destination\$($file.Name)"
+}
+
+Remove-Item -Path $mirror* -recurse
+
+
+#need to implement folder creation, and ability to pass in website url to the script 
